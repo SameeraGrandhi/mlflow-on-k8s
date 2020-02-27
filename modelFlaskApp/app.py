03d2e5b03436cc3883de8eca8a21f5b372b28e65
Srinivasan Lakshmi Ramalingam <srinivasan.ramalingam@imaginea.com>
2020-02-27 15:15:49 +0530

    fixed wrong config:

--- modelFlaskApp/app.py
+++ modelFlaskApp/app.py
@@ -3,12 +3,16 @@ Created on 13-Feb-2020
 
 @author: srinivasan
 '''
-import numpy as np
 from flask import Flask, request, jsonify, render_template
+import os
 import pickle
 
+import numpy as np
+
+model_path = os.path.join(os.environ['MODEL_PATH'], 'model.pkl')
+# model_path = 'model/model.pkl'
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open(model_path, 'rb'))
 
 
 @app.route('/')
@@ -18,22 +22,28 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in 
+                    request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-
     output = round(prediction[0], 2)
+    return render_template('index.html',
+                           prediction_text='Wine Quantity should be {}'.format(output))
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+
+@app.route('/predict_new', methods=['POST'])
+def predict_result():
+    print(request.get_json())
+    data = request.get_json()
+    out = model.predict(data)
+    return jsonify(out=out,
+                    status='success')
 
 
 @app.route('/results', methods=['POST'])
 def results():
-
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
-
     output = prediction[0]
     return jsonify(output)
 
